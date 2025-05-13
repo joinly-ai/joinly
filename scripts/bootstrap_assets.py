4708fa37e750c0bad3e1b642c1b244074a82f680
dbrockmann <dominiktobin@gmail.com>
2025-05-13 02:35:44 +0200

    add bootstrap script

---
+++ scripts/bootstrap_assets.py
@@ -0,0 +1,43 @@
+import os
+import pathlib
+import subprocess
+import urllib.request
+
+import torch
+from faster_whisper import WhisperModel
+
+
+def main() -> None:
+    """Download assets for the project."""
+    # download playwright browser
+    playwright_cmd = ["playwright", "install", "--no-shell", "chromium"]
+    subprocess.run(playwright_cmd, check=True)  # noqa: S603
+
+    # download whisper model
+    _ = WhisperModel(
+        "small",
+        device="cpu",
+        compute_type="int8",
+    )
+
+    # download silero vad model
+    _ = torch.hub.load("snakers4/silero-vad", "silero_vad", trust_repo=True)
+
+    # download kokoro model and voices
+    file_urls = [
+        "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/kokoro-v1.0.onnx",
+        "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files-v1.0/voices-v1.0.bin",
+    ]
+    cache_dir = (
+        pathlib.Path(os.getenv("XDG_CACHE_HOME", "~/.cache")).expanduser() / "kokoro"
+    )
+    cache_dir.mkdir(parents=True, exist_ok=True)
+    for url in file_urls:
+        fn = url.split("/")[-1]
+        dst = cache_dir / fn
+        if not dst.exists():
+            urllib.request.urlretrieve(url, dst)  # noqa: S310
+
+
+if __name__ == "__main__":
+    main()
