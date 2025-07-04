17bcd804be4e695887400fefc5a6007492e23a9a
dbrockmann <dominiktobin@gmail.com>
2025-07-04 01:27:52 +0000

    feat: add reset_settings function to restore previous settings

--- joinly/settings.py
+++ joinly/settings.py
@@ -67,3 +67,12 @@ def set_settings(settings: Settings) -> Token[Settings]:
         Token[Settings]: A token that can be used to reset the settings.
     """
     return _current_settings.set(settings)
+
+
+def reset_settings(token: Token[Settings]) -> None:
+    """Reset the current settings to the previous value.
+
+    Args:
+        token (Token[Settings]): The token returned by `set_settings`.
+    """
+    _current_settings.reset(token)
